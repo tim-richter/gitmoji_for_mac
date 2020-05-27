f33160fa124800ef9b0c59724cd7d33499716288
Tim Richter s80352 <s80352@beuth-hochschule.de>
2020-05-27 09:26:01 +0200

    Initial Commit

---
+++ gitmoji/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  gitmoji
+//
+//  Created by Tim Richter on 27.05.20.
+//  Copyright © 2020 Tim Richter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
